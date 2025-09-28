7921f0b9a41476d74ef8a439185985b2145cd2c6
luisfelipefr <lfghias232@gmail.com>
2025-09-28 17:34:51 -0300

    update: adicionar dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { AuthProvider } from './context/AuthContext'
+import ProtectedRoute from './components/ProtectedRoute'
 import Login from './pages/Login'
+import Dashboard from './pages/Dashboard'
 
 export default function App() {
   return (
@@ -8,6 +10,7 @@ export default function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/login" element={<Login />} />
+          <Route path="/" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
         </Routes>
       </BrowserRouter>
     </AuthProvider>
