e45564c0abdac86787b914b546cc5ae1f3ccfd16
luisfelipefr <lfghias232@gmail.com>
2025-09-28 17:27:42 -0300

    chore: atualizar Tailwind e adicionar vite-env.d.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,6 +2,10 @@ import type { Config } from 'tailwindcss'
 
 export default {
   content: ['./index.html', './src/**/*.{ts,tsx}'],
-  theme: { extend: {} },
+  theme: { extend: {
+    backgroundImage: {
+      'login-texture': "url('/src/assets/images/background-login.png')"
+    }
+  } },
   plugins: [],
 } satisfies Config
